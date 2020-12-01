0230dde562a6c311f3f0119e5263e226a7ad27ac
solidogen <solidogen@gmail.com>
2020-12-01 17:54:06 +0100

    Korge 2.0 update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import com.soywiz.korge.gradle.*
 
 buildscript {
+	val korgePluginVersion: String by project
+
 	repositories {
 		mavenLocal()
 		maven { url = uri("https://dl.bintray.com/korlibs/korlibs") }
@@ -9,7 +11,7 @@ buildscript {
 		google()
 	}
 	dependencies {
-		classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:2.0.0.2-rc2")
+		classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
 	}
 }
 
@@ -18,4 +20,5 @@ apply<KorgeGradlePlugin>()
 korge {
 	id = "com.spyrdonapps.metroidvania"
 	bundle("https://github.com/korlibs/korge-bundles.git::korge-box2d::4ac7fcee689e1b541849cedd1e017016128624b9##d15f5b28abbc2f58ba375d7204a6db2e90342978d02e675cd29249d0cade8f9f")
+	targetDefault()
 }
